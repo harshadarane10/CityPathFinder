d5d22cf5aad6d50bce26277ea0acbaf53c57bbea
harshadarane10 <65037496+harshadarane10@users.noreply.github.com>
2020-08-05 18:41:17 -0400

    Update CityPathFinderController.java

--- SpringProject/citypathfinder/src/main/java/com/harshadaproject/citypathfinder/CityPathFinderController.java
+++ SpringProject/citypathfinder/src/main/java/com/harshadaproject/citypathfinder/CityPathFinderController.java
@@ -1,5 +1,8 @@
 package com.harshadaproject.citypathfinder;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;  
@@ -19,71 +22,76 @@ public class CityPathFinderController {
 	HashMap<String,Set<String>> cityPathMap= new HashMap<>();
 	Queue<String> cityqueue = new LinkedList<String>();
 	HashSet<String> visitedCities = new HashSet<>();
+	Logger logger = LoggerFactory.getLogger(CityPathFinderController.class);
+	private String CONNECTED = "yes";
+	private String NOT_CONNECTED = "no";
 
 	
 	public void printCityMap() {		
-		System.out.println("Display CityMap : ");
-		for(String c : cityPathMap.keySet()) {
-			System.out.println("City "+c+"->"+cityPathMap.get(c).toString());
+		logger.trace("Display CityMap : ");
+		for(String c : cityPathMap.keySet()) {			
+			logger.trace("City "+c+"->"+cityPathMap.get(c).toString());
 		}		
 	}
 	
-	public void buildCityMap() {
+	public boolean buildCityMap() {
 		
 		try {
 			
-			String fileName = "city1.txt";
+			String fileName = "city.txt";
 	        ClassLoader classLoader = new CityPathFinderController().getClass().getClassLoader();
 	 
-	        System.out.println("File Name : " + fileName);
+	        logger.info("File Name : " + fileName);
+	        File file = null;
+	        if(classLoader.getResource(fileName) != null)
+	        	file = new File(classLoader.getResource(fileName).getFile());        
 	        
-	        File file = new File(classLoader.getResource(fileName).getFile());
+	             
+	        String content = null;
 	        
-	        System.out.println("File Object : " + file);
-	         
-	        //File is found
-	        System.out.println("File Found : " + file.exists());
-	         
-	        //Read File Content
-	        String content = new String(Files.readAllBytes(file.toPath()));
+	        if (file.exists()) {
+	        	//Read File Content
+	        	content = new String(Files.readAllBytes(file.toPath()));	        
+	        	String[] slines = content.split("\\n");        
+	        	String city1 = "";
+	        	String city2="";
 	        
-	        String[] slines = content.split("\\n");
-	        
-	        
-	        
-	        String city1 = "";
-	        String city2="";
-	        
-	        for(String s:slines) {
-	        	System.out.println(s);
-	        	if(s.trim() != "") {
-		        	String[] cities = s.split(",");	  
+	        	for(String s:slines) {
+	        		//logger.trace(s);
+	        		if(s.trim() != "") {
+	        			String[] cities = s.split(",");	  
 		        	
-		        	city1 =cities[0].trim();
-		        	city2 =cities[1].trim();
-		        	System.out.println("Source "+city1+"->"+"Dest"+city2);    	
+	        			city1 =cities[0].trim();
+	        			city2 =cities[1].trim();
+	        			//logger.trace("Source "+city1+"->"+"Dest"+city2);   		        	
 		        	
+	        			cityPathMap.putIfAbsent(city1, new HashSet<String>());
+	        			cityPathMap.get(city1).add(city2);		        	
+	        			//logger.trace("Entry added "+city1+ ":"+city2);
 		        	
-		        	cityPathMap.putIfAbsent(city1, new HashSet<String>());
-		        	cityPathMap.get(city1).add(city2);
-		        	
-		        	System.out.println("Entry added "+city1+ ":"+city2);
-		        	
-		        	cityPathMap.putIfAbsent(city2, new HashSet<String>());
-		        	cityPathMap.get(city2).add(city1);
-		        	System.out.println("Entry added "+city2+ ":"+city1);
+	        			cityPathMap.putIfAbsent(city2, new HashSet<String>());
+	        			cityPathMap.get(city2).add(city1);
+	        			//logger.trace("Entry added "+city2+ ":"+city1);
+	        		}
 	        	}
-	        }
-		}catch(IOException e) {
-			e.printStackTrace();			
+	       }else {
+	    	   logger.error("File does not exist");
+	    	   return false;
+	       }
+		
+	    }catch(IOException e) {
+			e.printStackTrace();	
+			logger.error("IOException "+e);
 		}catch(Exception e) {
-			e.printStackTrace();			
+			e.printStackTrace();	
+			logger.error("Exception "+e);
 		}
 		
+		return true;
 	}
 	
 	public String searchCityMap(String source,String Destination) {
-		String result="no";		
+		String result=NOT_CONNECTED;		
 		
 		cityqueue.add(source);		
 		while(!cityqueue.isEmpty()) {
@@ -91,13 +99,18 @@ public class CityPathFinderController {
 			// Check all connected cities 
 			// if match with dest then return yes
 			// if not in visited cities then add in queue
+			logger.trace("searchCity["+searchCity+"]");
+			
 			Set<String> cityset = cityPathMap.get(searchCity);
+			
+			logger.trace("cityset"+cityset);
 			
 			
 			if(cityset!= null) {
-				for(String c:cityset) {					
+				for(String c:cityset) {	
+					logger.trace("connected city =["+c+": Dest=["+Destination+"]");
 					if(c.equalsIgnoreCase(Destination)) {
-						return "yes";
+						return CONNECTED;
 					}
 					else {
 						if (!visitedCities.contains(c)) {
@@ -113,31 +126,23 @@ public class CityPathFinderController {
 		return result;
 		
 	}
-	/*
-	 * 	Boston, New York
-		Philadelphia, New York
-		Newark, Boston
-		Trenton, Albany
-	 */
-	/*
-	http://localhost:8080/connect?origin="Boston"&destination="New York"
-	http://localhost:8080/connect?origin=Boston&destination=Philadelphia
-	http://localhost:8080/connect?origin=Boston&destination=Albany
-	http://localhost:8080/connect?origin=Boston&destination=Trenton
-    */
+	
 	@RequestMapping("/connect")  
 	public String isCitiesConnected(@RequestParam(value="origin" ,required = false) String source,@RequestParam(value="destination", required = false) String dest)   
 	{  
-		String result = "no";
+		String result = NOT_CONNECTED;
 		
-		buildCityMap();
+		if (!buildCityMap())
+			return NOT_CONNECTED;
+			
 		printCityMap();
 		
 		if(source == null || dest == null) {
-			return "no";
+			return NOT_CONNECTED;
 		}
 		
 		result = searchCityMap(source.trim(),dest.trim());
+		logger.trace("result=["+result);
 		
 		return result;  
 	}  
